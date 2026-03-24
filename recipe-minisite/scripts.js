fa8f47814ab86b91b775d8cb22478d3d19923067
jennyparrish <jennifer.parrish@wsu.edu>
2026-03-23 23:12:00 -0700

    added fx to hide/show menu items when clicked

--- recipe-minisite/scripts.js
+++ recipe-minisite/scripts.js
@@ -1,5 +1,18 @@
 // JS for responsive nav menu
 
+// function to hide/show menu items
+function clickNav(bodyClicked) {
+    let burgerWasClicked = document.querySelector("nav").classList.contains("clicked");
+    document.querySelector("nav").classList.toggle("clicked");
+
+    // remove menu .clicked if burger is closed
+    if (burgerWasClicked || bodyClicked) {
+        let allMenus = document.querySelectorAll("nav > ul > li");
+        for (const eachMenu of allMenus) {
+            eachMenu.classList.remove("clicked");
+        }
+    }
+}
 // wait until DOM is loaded before we can start adding event listeners
 document.addEventListener("DOMContentLoaded", function() {
 
