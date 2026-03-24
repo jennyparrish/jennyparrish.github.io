1f469009236d129a8eaf7cfcf941be5811258e47
jennyparrish <jennifer.parrish@wsu.edu>
2026-03-23 22:43:02 -0700

    added clicked js

---
+++ recipe-minisite/scripts.js
@@ -0,0 +1,10 @@
+// JS for responsive nav menu
+
+// wait until DOM is loaded before we can start adding event listeners
+document.addEventListener("DOMContentLoaded", function() {
+
+    // respond to clicks on burger icon
+    document.querySelector("navBurger").addEventListener("clicked", function(e){
+        document.querySelector("nav").classList.toggle("clicked");
+    });
+});
