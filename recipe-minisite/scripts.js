7f9a687b2c770fa0cbd48ea8f28cddff2257ac38
jennyparrish <jennifer.parrish@wsu.edu>
2026-03-23 23:22:02 -0700

    added js handling to topline nav, loop thru

--- recipe-minisite/scripts.js
+++ recipe-minisite/scripts.js
@@ -18,6 +18,25 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // respond to clicks on burger icon
     document.querySelector("#navBurger").addEventListener("click", function(e){
-        document.querySelector("nav").classList.toggle("clicked");
+        clickNav(false);
     });
+
+    // handles to all topline nav items
+    let allMenus = document.querySelectorAll("nav > ul > li");
+    for (const eachMenu of allMenus) {
+        // loop through collection of handles individually
+        eachMenu.addEventListener("click", function(e) {
+            let wasClicked = eachMenu.classList.contains("clicked");
+            let allMenus2 = document.querySelectorAll("nav > ul > li");
+            for (const eachMenu2 of allMenus2) {
+                eachMenu2.classList.remove("clicked");
+            }
+            if (!wasClicked) {
+                eachMenu.classList.add("clicked");      // if this is newly clicked, add click class back
+            }
+        });
+    }
 });
+
+
+// document.querySelector("nav").classList.toggle("clicked");
