8a862dcd9248aaa96722cdc345dc8d983d9b7e2d
jennyparrish <jennifer.parrish@wsu.edu>
2026-04-01 21:41:49 -0700

    troubleshooting js problems

--- dark-flower/scripts.js
+++ dark-flower/scripts.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     let darkModeButton = document.querySelector("#toggleDarkMode");
     darkModeButton.addEventListener("click", function(e) {
-        document.body.classList.toggle("darkMode");
+        document.div.classList.toggle("darkMode");
     });
 
     });
