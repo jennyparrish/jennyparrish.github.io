baffdfa266047542f9c1f127149b9e63062f3c28
jennyparrish <jennifer.parrish@wsu.edu>
2026-04-01 19:06:32 -0700

    added click js for darkmode button

--- dark-flower/scripts.js
+++ dark-flower/scripts.js
@@ -1,10 +1,20 @@
-let oneItem = document.querySelector("#firstObject");
-let twoItem = document.querySelector("#secondObject");
+// wait for the DOM to load completely
+document.addEventListener("DOMContentLoaded", function() {
 
-oneItem.aaddEventListener("mouseover", function(e) {
-    twoItem.classList.add("gotMoused");  // add class
+    let darkModeButton = document.querySelector("#toggleDarkMode");
+    darkModeButton.addEventListener("click", function(e) {
+        document.body.classList.toggle("daarkMode");
+    });
+
+    let oneItem = document.querySelector("#firstObject");
+    let twoItem = document.querySelector("#secondObject");
+
+    oneItem.addEventListener("mouseover", function (e) {
+        twoItem.classList.add("gotClicked");  // add class
+    });
+
+    oneItem.addEventListener("mouseout", function (e) {
+        twoItem.classList.remove("gotMoused");  // remove class
+    });
+
 });
-
-oneItem.addEventListener("mouseout", function(e) {
-    twoItem.classList.remove("gotMoused");  // remove class
-})
