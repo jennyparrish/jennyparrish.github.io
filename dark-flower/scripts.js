9a3cded725b5014365f9cafd2bf03d2ef6be2ee1
jennyparrish <jennifer.parrish@wsu.edu>
2026-04-01 22:04:45 -0700

    updated darkmode for flowerbkgd

--- dark-flower/scripts.js
+++ dark-flower/scripts.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     let darkModeButton = document.querySelector("#toggleDarkMode");
-    darkModeButton.addEventListener("click", function(e) {
+    darkModeButton.addEventListener("click", function(e){
         document.body.classList.toggle("darkMode");
     });
 
